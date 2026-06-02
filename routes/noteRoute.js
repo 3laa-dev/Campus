4dc5066b50c0b3ac30a115037b40b5f575fe6298
Alaaelden <alaahhee05@gmail.com>
2026-06-02 09:20:08 +0300

    fdsa

--- routes/noteRoute.js
+++ routes/noteRoute.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const controller = require("../controllers/noteController");
-
+const auth = require("../controllers/authController");
 
 const multer = require("multer");
 const guid = require("guid");
@@ -8,33 +8,57 @@ const path = require("path");
 
 const diskStorage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const type = file.mimetype.split("/")[0]
-    if(type === "application")
-        cb(null, "uploads/files");
-     else
-         cb(new Error("Only files are allowed"), null);
+    const type = file.mimetype.split("/")[0];
+
+    if (type === "application")
+      cb(null, "uploads/files");
+    else
+      cb(new Error("Only files are allowed"), null);
   },
 
   filename: function (req, file, cb) {
     const ext = path.extname(file.originalname);
-
     cb(null, guid.create().value + ext);
   },
 });
 
 const up = multer({ storage: diskStorage });
 
-router.route("/")
-    .post(up.single("file"),
-    (req , res , next)=>{
-      req.body.file=req.file.filename;
+router
+  .route("/")
+  .post(
+    auth.protect,
+    auth.allowedTo("student"),
+    up.single("file"),
+    (req, res, next) => {
+      req.body.file = req.file.filename;
       req.body.user = req.user._id;
-      next()}  , controller.createNote)
-    .get(controller.getAllNotes);
+      next();
+    },
+    controller.createNote
+  )
+  .get(
+    auth.protect,
+    auth.allowedTo("student"),
+    controller.getAllNotes
+  );
 
-router.route("/:id")
-    .get(controller.getNote)
-    .put(controller.updateNote)
-    .delete(controller.deleteNote);
+router
+  .route("/:id")
+  .get(
+    auth.protect,
+    auth.allowedTo("student"),
+    controller.getNote
+  )
+  .put(
+    auth.protect,
+    auth.allowedTo("student"),
+    controller.updateNote
+  )
+  .delete(
+    auth.protect,
+    auth.allowedTo("student", "admin"),
+    controller.deleteNote
+  );
 
 module.exports = router;
