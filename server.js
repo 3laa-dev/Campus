9a8c4ac391872df560edd9c0c186066d4299c1a9
osamaakil4 <osamaakil4@gmail.com>
2026-06-09 17:13:44 +0300

    test deleted

--- server.js
+++ server.js
@@ -48,7 +48,9 @@ app.use("/comment" ,commentRoute);
 app.use("/messages",messagesRoute);
 app.use("/search", searchRoute);
 app.use("/stats", statsRoute);
-
+app.use("/test", (req, res) => {
+  res.json({ message: "test merhsnkkln  g" });
+}
 
 
 
